f1835e409863beb459f049b0c750c7c1de508b2b
NizarMjr <nizar.mjr94@gmail.com>
2026-03-13 14:23:34 +0100

    update

--- frontend/src/components/AddComments.js
+++ frontend/src/components/AddComments.js
@@ -43,28 +43,33 @@ const AddComments = ({ bookId }) => {
             console.log("Error submitting comment", err);
         }
     };
-    return (
-        < div className="mt-12 bg-white shadow-md rounded-xl p-8 max-w-4xl mx-auto" >
-            {alert.show && <AlertMessage message={alert.message} type={alert.type} onClose={handleCloseAlert} />}
-            <h2 className="text-2xl font-bold mb-6 flex items-center gap-2">
-                <FaComment className="text-slate-600" /> التعليقات والمراجعات
-            </h2>
 
-            <form onSubmit={handleCommentSubmit} className="space-y-4">
-                <textarea
-                    value={comment}
-                    onChange={(e) => setComment(e.target.value)}
-                    placeholder="شاركنا رأيك في هذا الكتاب..."
-                    className="w-full p-4 border-2 border-slate-100 rounded-lg focus:border-blue-500 outline-none transition resize-none h-32"
-                ></textarea>
-                <button
-                    type="submit"
-                    className="bg-slate-800 text-white py-2 px-8 rounded-lg font-bold hover:bg-black transition"
-                >
-                    إرسال التعليق
-                </button>
-            </form>
-        </div >
+    return (
+        <>
+            {alert.show && <AlertMessage message={alert.message} type={alert.type} onClose={handleCloseAlert} />}
+
+            < div className="mt-12 bg-white shadow-md rounded-xl p-8 max-w-4xl mx-auto" >
+                <h2 className="text-2xl font-bold mb-6 flex items-center gap-2">
+                    <FaComment className="text-slate-600" /> التعليقات والمراجعات
+                </h2>
+
+                <form onSubmit={handleCommentSubmit} className="space-y-4">
+                    <textarea
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                        placeholder="شاركنا رأيك في هذا الكتاب..."
+                        className="w-full p-4 border-2 border-slate-100 rounded-lg focus:border-blue-500 outline-none transition resize-none h-32"
+                    ></textarea>
+                    <button
+                        type="submit"
+                        className="bg-slate-800 text-white py-2 px-8 rounded-lg font-bold hover:bg-black transition"
+                    >
+                        إرسال التعليق
+                    </button>
+                </form>
+            </div >
+        </>
+
     )
 }
 export default AddComments;
